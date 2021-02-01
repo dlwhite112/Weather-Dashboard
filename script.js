a65d6ffeeebd68ea765356e776985c2152d96fb5
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2021-02-01 11:29:13 -0600

    add function tests for 5 day forcast api

--- script.js
+++ script.js
@@ -37,3 +37,35 @@ fetch(currentUvUrl)
   .then(function (data) {
       console.log('UV Index: ' + data.value)
   })
+
+  //   5 day forecast try 1 
+// var fiveDayUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Austin&appid=541084353f3ae604f81c495b436c8636&units=imperial'
+
+// fetch(fiveDayUrl)
+// .then(function (response) {
+//     return response.json();
+// })
+// .then(function (data) {
+//     console.log('Fetch Response for the 5 day \n--------------')
+//     console.log(data.list[3,]);
+// })
+
+// 5 day forecast try 2
+var fiveDayUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=30.26&lon=-97.74&exclude=minutely,hourly,alerts,current&appid=541084353f3ae604f81c495b436c8636&units=imperial'
+
+fetch(fiveDayUrl)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+  // WILL HAVE TO LOOP THROUGH THIS FOR 5 DAYS
+    console.log('Fetch Response for the 5 day \n--------------')
+    // date (dt timestamp HAVE TO CONVERT THIS TO DAY)
+    console.log(data.daily[0].dt)
+    // icon
+    console.log(data.daily[0].weather[0].icon)
+    // temp
+    console.log('Temperature: ' + data.daily[0].temp.day + '°F')
+    // humidity
+    console.log('Humidity: ' + data.daily[0].humidity + '%')
+})
