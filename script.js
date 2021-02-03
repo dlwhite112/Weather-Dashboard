e4ddee0f80f975fa03b1907a4fb407b45439bf29
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2021-02-03 16:56:46 -0600

    add function to 5 day forecast

--- script.js
+++ script.js
@@ -5,18 +5,17 @@ var currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Austi
 var searchBtn = document.getElementById('submit');
 var citySearch = document.getElementById('search');
 
-// VARS FOR MAIN CARD APPEND
-var mainCardTemp = document.getElementById("mainCardTemp");
-var mainCardHumidity = document.getElementById("mainCardHumidity");
-var mainCardWindSpeed = document.getElementById("mainCardWindSpeed");
-var MainCardUVIndex = document.getElementById("mainCardUVIndex");
+// // VARS FOR MAIN CARD APPEND
+// var mainCardTemp = document.getElementById("mainCardTemp");
+// var mainCardHumidity = document.getElementById("mainCardHumidity");
+// var mainCardWindSpeed = document.getElementById("mainCardWindSpeed");
+// var MainCardUVIndex = document.getElementById("mainCardUVIndex");
 
 
 
 // User Input City Selection
 
 
-// Vars for Main Card
 
 
 
@@ -112,10 +111,118 @@ fetch(fiveDayUrl)
     return response.json();
 })
 .then(function (data) {
-  // WILL HAVE TO LOOP THROUGH THIS FOR 5 DAYS
+    // WILL HAVE TO LOOP THROUGH THIS FOR 5 DAYS
     console.log('Fetch Response for the 5 day \n--------------')
+
+    // for ( i = 1; i <= 5; i++) {
+    //   const element = (data.daily)[i];
+      
+    //   console.log(element)
+      
+    // }
+
+
     // date (dt timestamp HAVE TO CONVERT THIS TO DAY)
-    console.log(data.daily[0].dt)
+
+    // DAY 1 FORECAST
+    var day1date = document.getElementById("card-body1")
+    var day1 = document.createElement('h5');
+    var day1icon = document.createElement('p')
+        day1icon.textContent = data.daily[1].weather[0].icon;
+    var day1Temp = document.createElement('p')
+        day1Temp.textContent = 'Temp: ' + data.daily[1].temp.day + '°F';
+    var day1Humidity = document.createElement('p')
+        day1Humidity.textContent = 'Humidity: ' + data.daily[1].humidity + '%';
+    var firstDate = new Date(data.daily[1].dt * 1000);
+        dateString1 = firstDate.toGMTString();
+        console.log(dateString1);
+
+          day1.textContent = dateString1;
+          day1date.appendChild(day1);
+          day1date.appendChild(day1icon);
+          day1date.appendChild(day1Temp)
+          day1date.appendChild(day1Humidity)
+         
+
+    // DAY 2 FORCAST
+    var day2date = document.getElementById("card-body2")
+    var day2 = document.createElement('h5');
+    var day2icon = document.createElement('p')
+        day2icon.textContent = data.daily[2].weather[0].icon;
+    var day2Temp = document.createElement('p')
+        day2Temp.textContent = 'Temp: ' + data.daily[2].temp.day + '°F';
+    var day2Humidity = document.createElement('p')
+        day2Humidity.textContent = 'Humidity: ' + data.daily[2].humidity + '%';
+    var firstDate = new Date(data.daily[2].dt * 1000);
+        dateString2 = firstDate.toGMTString();
+        console.log(dateString2);
+
+          day2.textContent = dateString2;
+          day2date.appendChild(day2);
+          day2date.appendChild(day2icon);
+          day2date.appendChild(day2Temp)
+          day2date.appendChild(day2Humidity)
+
+    // DAY 3 FORCAST
+    var day3date = document.getElementById("card-body3")
+    var day3 = document.createElement('h5');
+    var day3icon = document.createElement('p')
+        day3icon.textContent = data.daily[3].weather[0].icon;
+    var day3Temp = document.createElement('p')
+        day3Temp.textContent = 'Temp: ' + data.daily[3].temp.day + '°F';
+    var day3Humidity = document.createElement('p')
+        day3Humidity.textContent = 'Humidity: ' + data.daily[3].humidity + '%';
+    var firstDate = new Date(data.daily[3].dt * 1000);
+        dateString3 = firstDate.toGMTString();
+        console.log(dateString3);
+
+          day3.textContent = dateString3;
+          day3date.appendChild(day3)
+          day3date.appendChild(day3icon);
+          day3date.appendChild(day3Temp)
+          day3date.appendChild(day3Humidity)
+
+    // DAY 4 FORCAST
+    var day4date = document.getElementById("card-body4")
+    var day4 = document.createElement('h5');
+    var day4icon = document.createElement('p')
+        day4icon.textContent = data.daily[4].weather[0].icon;
+    var day4Temp = document.createElement('p')
+        day4Temp.textContent = 'Temp: ' + data.daily[4].temp.day + '°F';
+    var day4Humidity = document.createElement('p')
+        day4Humidity.textContent = 'Humidity: ' + data.daily[4].humidity + '%';
+    var firstDate = new Date(data.daily[4].dt * 1000);
+        dateString4 = firstDate.toGMTString();
+        console.log(dateString4);
+
+          day4.textContent = dateString4;
+          day4date.appendChild(day4)
+          day4date.appendChild(day4icon);
+          day4date.appendChild(day4Temp)
+          day4date.appendChild(day4Humidity)
+
+
+    // DAY 5 FORCAST
+    var day5date = document.getElementById("card-body5")
+    var day5 = document.createElement('h5');
+    var day5icon = document.createElement('p')
+        day5icon.textContent = data.daily[5].weather[0].icon;
+    var day5Temp = document.createElement('p')
+        day5Temp.textContent = 'Temp: ' + data.daily[5].temp.day + '°F';
+    var day5Humidity = document.createElement('p')
+        day5Humidity.textContent = 'Humidity: ' + data.daily[5].humidity + '%';
+    var firstDate = new Date(data.daily[5].dt * 1000);
+        dateString5 = firstDate.toGMTString();
+        console.log(dateString5);
+
+          day5.textContent = dateString5;
+          day5date.appendChild(day5)
+          day5date.appendChild(day5icon);
+          day5date.appendChild(day5Temp)
+          day5date.appendChild(day5Humidity)
+
+
+
     // icon
     console.log(data.daily[0].weather[0].icon)
     // temp
@@ -127,5 +234,5 @@ fetch(fiveDayUrl)
 
 // searchBtn.addEventListener('click', ;
 
-console.log('Local Storage Functionality')
-console.log(localStorage)
+// console.log('Local Storage Functionality')
+// console.log(localStorage)
